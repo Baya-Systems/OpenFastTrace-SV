1e6bdbedf0e26a168135482ed26bd435a957fb8e
Eric Norige <eric@bayasystems.com>
2025-10-17 16:40:52 -0700

    Add .inc extension support

--- src/main/java/com/bayasystems/oft/svimporter/util/SVConstants.java
+++ src/main/java/com/bayasystems/oft/svimporter/util/SVConstants.java
@@ -6,5 +6,5 @@ public class SVConstants {
     public static final String DEFAULT_TITLE = "Untitled";
     public static final String CONFIG_FILE_PATH = "config/sv_importer_config.json";
     public static final String VERSION = "1.0.0";
-    public static final String[] supportedFilenamePatterns = { ".sv", ".v" };
+    public static final String[] supportedFilenamePatterns = { ".sv", ".v", ".inc" };
 }
